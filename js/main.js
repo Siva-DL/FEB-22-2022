b66518aafcb382fa56d8c6f13afccfc7e33bedf1
Siva-DL <sivaprasad.digitallync@gmail.com>
2022-03-16 10:36:04 +0530

    JS: Intro, Variabls and Arthemetic Operators.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// alert(demo123);
+console.log(12312123);
+
+// DataTypes
+// String, numbers, boolean, null, undefined;
+
+console.log("Demo1##$@#@23");
+("1234");
+
+("");
+("");
+
+1231233;
+true;
+false;
+null;
+undefined;
+
+// Objects(Arrays) and functions,
---
+++ js/variables.js
@@ -0,0 +1,27 @@
+// Variables:
+
+// String, Number, Boolean, null, undefined.
+// Array. object, functions
+
+var userName = "null";
+
+console.log("*^&*^1LJKLKJ231ASASA2312");
+console.log(12312312);
+console.log(true);
+
+var test;
+
+// null - empty value
+// undefined - no value for var
+console.log(test);
+
+//  + - * /
+console.log(10 + 10); // 20
+
+console.log(("Demo" * 20) / 10); // 1030 - concatination // string / number => NaN
+
+// typeof
+
+console.log(typeof null); // string
+
+// > < >= <= == and ===
